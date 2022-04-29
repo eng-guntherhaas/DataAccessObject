59cb8fe7ee4f75925c9d9e4ca6a60fd0834434c7
Gunther Haas <guntherhaas.eng@gmail.com>
2022-04-29 16:32:52 -0300

    Create insert method to SellerDaoJDBC

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public interface SellerDao {
 
-    void insert(Seller seller);
-    void update(Seller seller);
+    void insert(Seller obj);
+    void update(Seller obj);
     void deleteById(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
